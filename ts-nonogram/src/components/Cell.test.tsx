fad749103d43c5b2f95b6bf38fe280c7153eda65
Jose Medina <josemedina921@gmail.com>
2017-09-06 22:33:44 -0700

    got initial cell working

--- ts-nonogram/src/components/Cell.test.tsx
+++ ts-nonogram/src/components/Cell.test.tsx
@@ -1,7 +1,19 @@
+import { shallow, ShallowWrapper } from 'enzyme';
+import React from 'react';
 import Cell from './Cell';
 
 describe('Cell', () => {
-  it('should render the individual cell', () => {
-    expect(true).toBe(false);
+  describe('render', () => {
+    let wrapper: ShallowWrapper;
+
+    beforeEach(() => {
+      wrapper = shallow(
+        <Cell />,
+      );
+    });
+
+    it('should display hello world', () => {
+      expect(wrapper.text()).toEqual('Hello world');
+    });
   });
 });
