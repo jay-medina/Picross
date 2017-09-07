9067b13efe72b07b3551f6bb64ba9e17ae6e15d2
Jose Medina <josemedina921@gmail.com>
2017-09-06 22:18:24 -0700

    updating the entry point

--- ts-nonogram/src/index.tsx
+++ ts-nonogram/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Cell from './components/Cell';
+import Cell from './components/Cell';
 
 const mainContainer = document.querySelector('#main-container');
 
-ReactDOM.render(<div>Test</div> , mainContainer);
+ReactDOM.render(<Cell /> , mainContainer);
--- ts-nonogram/webpack.config.js
+++ ts-nonogram/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: './src/index.tsx',
   output: {
     filename: 'app/index.js',
     path: path.join(__dirname, 'dist'),
