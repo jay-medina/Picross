18a4955a88c1d4201884861452cb817cd80e002a
Jose Medina <josemedina921@gmail.com>
2017-09-10 21:01:22 -0700

    update cell unit test

--- ts-nonogram/src/components/Cell/index.test.tsx
+++ ts-nonogram/src/components/Cell/index.test.tsx
@@ -3,17 +3,40 @@ import React from 'react';
 import Cell from '.';
 
 describe('Cell', () => {
-  describe('render', () => {
-    let wrapper: ShallowWrapper;
+  let wrapper: ShallowWrapper;
 
+  describe('by default', () => {
     beforeEach(() => {
       wrapper = shallow(
         <Cell />,
       );
     });
 
-    it('should display hello world', () => {
-      expect(wrapper.text()).toEqual('Hello world');
+    it('should have className cell', () => {
+      expect(wrapper.hasClass('cell')).toBe(true);
+    });
+  });
+
+  describe('selected', () => {
+    beforeEach(() => {
+      wrapper = shallow(
+        <Cell state="selected" />,
+      );
+    });
+
+    it('should select the cell', () => {
+      expect(wrapper.find('.selected').length).toBe(1);
+    });
+  });
+
+  describe('crossed', () => {
+    beforeEach(() => {
+      wrapper = shallow(
+        <Cell state="crossed" />,
+      );
+    });
+    it('should cross out the cell', () => {
+      expect(wrapper.find('.cross').length).toBe(1);
     });
   });
 });
